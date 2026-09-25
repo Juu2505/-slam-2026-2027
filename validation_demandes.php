e3569a42fe77c8d1274358142a1c0e65a545a61a
Juu2505 <julie.myriam.durand@gmail.com>
2026-09-25 16:32:39 +0200

    d

--- validation_demandes.php
+++ validation_demandes.php
@@ -12,55 +12,34 @@ $demandes = array(
     array("id" => 103, "etat" => "nouvelle", "montant" => 1400)
 );
  
-$compteur = 0;
- 
-foreach ($demandes as $demande) {
-    if (peutValider($utilisateur, $demande)) {
-        $compteur++;
-        echo "Demande ID: " . $demande["id"] . " est valide pour traitement.<br>";
-    }
-    else {
-        echo "Demande ID: " . $demande["id"] . " n'est pas valide pour traitement.<br>";
-    }
-}
- 
-echo "Nombre total de demandes valides pour traitement: " . $compteur . "<br>";
+
 
 function peutValider(array $utilisateur, array $demande): bool 
 { 
-    return (
-        $demande["etat"] === "nouvelle"
-        && $utilisateur["actif"] === true
-        && ($utilisateur["role"] === "administrateur"
-        || ($utilisateur["role"] === "gestionnaire"
-        && $demande["montant"] <= 1000))
-    );
-}
+    if(!$utilisateur["actif"])
+        {
+            return false;
+        }
+    if(!$demande["etat"] === "nouvelle")
+        {
+            return false;
+        }
+            return $utilisateur["role"] === "administrateur" || ($utilisateur["role"] === "gestionnaire"
+        && $demande["montant"] <= 1000);
+    
+} 
 
 function compterDemandesValidables(array $utilisateur, array $demandes): int 
 { 
-    $compteur = 0;
-    
-    foreach ($demandes as $demande) {
-        if (peutValider($utilisateur, $demande)) {
-            $compteur++;
-        }
-    }
-    return $compteur;
-} 
-
-function peutValider(array $utilisateu,array $demande): bool
-if (!$utilisateur["actif"])
-    return false;
-
-if(
-    $demande["etat"] === "nouvelle")
-    return true;
-
-return $utilisateu["role"] === "administrateur" 
-||
- $utilisateu["role"] === "gestionnaire" 
- && $demande["montant"] <=1000
+$compteur = 0;
  
- ;
+foreach ($demandes as $demande) {
+if (peutValider($utilisateur, $demande)) { 
+        $compteur++;
+}
+}
+
+return $compteur;
+}
+ 
 ?>
